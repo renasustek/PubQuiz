8570914b8de8ebf9e41c0920b5be95ecd4a18745
renasustek <renasustek@gmail.com>
2026-04-21 18:41:22 +0100

    first commit

---
+++ src/main/java/com/github/renas/PubQuiz/PubQuizApplication.java
@@ -0,0 +1,13 @@
+package com.github.renas.PubQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/renas/PubQuiz/PubQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renas.PubQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
